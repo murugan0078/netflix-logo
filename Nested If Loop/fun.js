cd96d005c120d65f3010683d01996e01dc915494
murugan0078 <murugan.t0078@gmail.com>
2022-12-31 11:24:09 +0530

    my projects

---
+++ Nested If Loop/fun.js
@@ -0,0 +1,7 @@
+var tamil =Number (prompt(tamil))
+var english =Number (prompt(english))
+var result = calculate(tamil,english)
+function calculate(tamil_mark, english_mark){
+    return tamil_mark + english_mark
+
+}
